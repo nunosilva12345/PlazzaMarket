4ccc844a0b432824dcc93c5d35b4c03cfda5412d
lengors <pedrocavadinhas@gmail.com>
2019-05-23 17:28:18 +0100

    Producer and Consumer

--- src/main/java/ua/tqs/g6/project/controllers/ConsumerController.java
+++ src/main/java/ua/tqs/g6/project/controllers/ConsumerController.java
@@ -1,116 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ua.tqs.g6.project.controllers;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 import java.lang.reflect.InvocationTargetException;
+
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import ua.tqs.g6.project.entities.Consumer;
-import ua.tqs.g6.project.entities.Product;
-import ua.tqs.g6.project.entities.Sales;
-import ua.tqs.g6.project.repositories.ConsumerRepository;
-import ua.tqs.g6.project.repositories.ProductRepository;
-import ua.tqs.g6.project.utils.Status;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import ua.tqs.g6.project.entities.Consumer;
 import ua.tqs.g6.project.repositories.ConsumerRepository;
 
 @Controller
 @RequestMapping(path = "/consumer")
-@Api(value = "Consumer", description = "Operations with entity consumer")
+@Api(value = "Consumer", description="Operations with entity consumer")
 public class ConsumerController
 {
-	@Autowired
-	private ConsumerRepository consumerRepo;
-	
-	@Autowired
-	private ProductRepository productRepo;
+    @Autowired
+    private ConsumerRepository consumerRepository;
 
-	@ApiOperation(value = "Get all consumers")
-	@CrossOrigin(origins = "*")
-	@GetMapping
-	public @ResponseBody Iterable<Consumer> findAll()
-	{
-		return consumerRepo.findAll();
-	}
-
-	@ApiOperation(value = "Get consumer by id")
-	@GetMapping(path = "/{userId}")
-	public ResponseEntity<Consumer> find(@PathVariable("userId") int userId)
-	{	
-		Optional<Consumer> consumer = consumerRepo.findById(userId);
-		if (consumer.isPresent())
-			return new ResponseEntity<>(consumer.get(), HttpStatus.OK);
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-	}
-
-	@ApiOperation(value = "Add consumer to database")
+    @ApiOperation(value = "Add consumer to database")
 	@PostMapping(consumes = "application/json")
 	public @ResponseBody Consumer create(@RequestBody Consumer consumer)
 	{
-		return consumerRepo.saveAndFlush(consumer);
-	}
-	
-	@PutMapping(path = "/{id}", consumes = "application/json")
-	public ResponseEntity<Object> update(@RequestBody Map<String, Object> json, @PathVariable("id") int id)
-	{
-		Optional<Consumer> consumer = consumerRepo.findById(id);
+		return consumerRepository.saveAndFlush(consumer);
+    }
+    
+    @DeleteMapping(path = "/{id}")
+    @ApiOperation(value = "Delete consumer by id")
+    public ResponseEntity<Consumer> delete(@PathVariable("id") int id)
+    {
+        Optional<Consumer> consumer = consumerRepository.findById(id);
+        if (consumer.isPresent())
+        {
+            consumerRepository.delete(consumer.get());
+		    return new ResponseEntity<>(consumer.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "*")
+    @ApiOperation(value = "Get all consumers")
+    public @ResponseBody Iterable<Consumer> findAll()
+    {
+        return consumerRepository.findAll();
+    }
+
+	@GetMapping(path = "/{id}")
+    @ApiOperation(value = "Get consumer by id")
+	public ResponseEntity<Consumer> findById(@PathVariable("id") int id)
+	{	
+		Optional<Consumer> consumer = consumerRepository.findById(id);
 		if (consumer.isPresent())
-		{
-			for (Map.Entry<String, Object> entry : json.entrySet())
-				try
-				{
-					consumer.get().getClass().getMethod(String.format("set%s",   entry.getKey().substring(0, 1).toUpperCase() + entry.getKey().substring(1)), entry.getValue().getClass()).invoke(consumer.get(), entry.getValue());
-				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
-						| NoSuchMethodException | SecurityException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			consumerRepo.saveAndFlush(consumer.get());	
-		}
+			return new ResponseEntity<>(consumer.get(), HttpStatus.OK);
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}
+    }
 
-	/*@ApiOperation(value = "Add product to cart")
-	@PutMapping(path = "/{userId}/cart/{productId}")
-	public ResponseEntity<Sales> addToCart(@PathVariable("userId") int userId, @PathVariable("productId") int productId)
+    @PutMapping(path = "/{id}", consumes = "application/json")
+    @ApiOperation(value = "Update selected consumer by id in database")
+	public ResponseEntity<Consumer> update(@RequestBody Map<String, Object> json, @PathVariable("id") int id)
 	{
-		Optional<Consumer> consumer = consumerRepo.findById(userId);
-		Optional<Product> product = productRepo.findById(productId);
-		if (consumer.isPresent() && product.isPresent())
-		{
-
-		}
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		Optional<Consumer> optional = consumerRepository.findById(id);
+        if (!optional.isPresent())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Consumer consumer = optional.get();
+		for (Map.Entry<String, Object> entry : json.entrySet())
+			try
+			{
+                String key = entry.getKey();
+                Object value = entry.getValue();
+				Consumer.class.getMethod(String.format("set%s", key.substring(0, 1).toUpperCase() + key.substring(1)), value.getClass()).invoke(consumer, value);
+            } 
+            catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+			{
+            	e.printStackTrace();
+			}
+		return new ResponseEntity<>(consumerRepository.saveAndFlush(consumer), HttpStatus.OK);
 	}
-
-	@ApiOperation(value = "Get product in cart")
-	@GetMapping(path = "/{userId}/cart")
-	public ResponseEntity<Iterable<Sales>> getCartList(@PathVariable("userId") int userId)
-	{
-		Optional<Consumer> consumer = consumerRepo.findById(userId);
-		if (consumer.isPresent())
-			return new ResponseEntity<>(consumer.get().getSales().stream()
-					.filter(s -> s.getStatus() == Status.PROCESSING).collect(Collectors.toList()), HttpStatus.OK);
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	}*/
 }
--- src/main/java/ua/tqs/g6/project/controllers/ProducerController.java
+++ src/main/java/ua/tqs/g6/project/controllers/ProducerController.java
@@ -1,50 +1,101 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ua.tqs.g6.project.controllers;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.InvocationTargetException;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import ua.tqs.g6.project.entities.Category;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import ua.tqs.g6.project.entities.Producer;
-import ua.tqs.g6.project.repositories.CategoryRepository;
 import ua.tqs.g6.project.repositories.ProducerRepository;
 
 @Controller
 @RequestMapping(path = "/producer")
-@Api(value="Producer", description="Operations with entity producer")
-public class ProducerController {
-    
+@Api(value = "Producer", description="Operations with entity producer")
+public class ProducerController
+{
     @Autowired
-    private ProducerRepository producerRepo;
-    
-    @ApiOperation(value = "Get all procucers")
-    @CrossOrigin(origins = "*")
-    @GetMapping
-    public @ResponseBody Iterable<Producer> findAll() {
-        return producerRepo.findAll();
-    }
-    
-    @ApiOperation(value = "Get producer by id")
-    @GetMapping(path = "/{id}")
-    public Producer find(@PathVariable("id") int id) {
-        Producer producer = null;
-        producer = producerRepo.getOne(id);
-        return producer;
-       
-    }
-    
+    private ProducerRepository producerRepository;
+
     @ApiOperation(value = "Add producer to database")
-    @PostMapping(consumes = "application/json")
-    public Producer create(@RequestBody Producer producer) {
-        return producerRepo.save(producer);
+	@PostMapping(consumes = "application/json")
+	public @ResponseBody Producer create(@RequestBody Producer producer)
+	{
+		return producerRepository.saveAndFlush(producer);
     }
     
+    @DeleteMapping(path = "/{id}")
+    @ApiOperation(value = "Delete producer by id")
+    public ResponseEntity<Producer> delete(@PathVariable("id") int id)
+    {
+        Optional<Producer> producer = producerRepository.findById(id);
+        if (producer.isPresent())
+        {
+            producerRepository.delete(producer.get());
+		    return new ResponseEntity<>(producer.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "*")
+    @ApiOperation(value = "Get all producers")
+    public @ResponseBody Iterable<Producer> findAll()
+    {
+        return producerRepository.findAll();
+    }
+
+	@GetMapping(path = "/{id}")
+    @ApiOperation(value = "Get producer by id")
+	public ResponseEntity<Producer> findById(@PathVariable("id") int id)
+	{	
+		Optional<Producer> producer = producerRepository.findById(id);
+		if (producer.isPresent())
+			return new ResponseEntity<>(producer.get(), HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(path = "/{id}", consumes = "application/json")
+    @ApiOperation(value = "Update selected producer by id in database")
+	public ResponseEntity<Producer> update(@RequestBody Map<String, Object> json, @PathVariable("id") int id)
+	{
+		Optional<Producer> optional = producerRepository.findById(id);
+        if (!optional.isPresent())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Producer producer = optional.get();
+		for (Map.Entry<String, Object> entry : json.entrySet())
+			try
+			{
+                String key = entry.getKey();
+                Object value = entry.getValue();
+				Producer.class.getMethod(String.format("set%s", key.substring(0, 1).toUpperCase() + key.substring(1)), value.getClass()).invoke(producer, value);
+            } 
+            catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+			{
+            	e.printStackTrace();
+			}
+		return new ResponseEntity<>(producerRepository.saveAndFlush(producer), HttpStatus.OK);
+	}
 }
