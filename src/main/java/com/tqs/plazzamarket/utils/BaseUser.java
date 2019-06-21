75e164fb1572635b7e1a5dd3c9dc065596f7f861
lengors <pedrocavadinhas@gmail.com>
2019-06-21 15:54:37 +0100

    Entities

---
+++ src/main/java/com/tqs/plazzamarket/utils/BaseUser.java
@@ -0,0 +1,62 @@
+package com.tqs.plazzamarket.utils;
+
+import javax.persistence.Id;
+
+public abstract class BaseUser {
+    @Id
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+    private String address;
+    private String zipCode;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return this.zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+}
