c583b28523f088c414a21282eb37921b3808b04d
lengors <pedrocavadinhas@gmail.com>
2019-06-28 23:17:50 +0100

    No target

--- target/classes/static/js/plazza-market/signup.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('select#accountType').change(function (event) {
-        if (event.target.selectedOptions[0].value == 'producer')
-            $('#website-container').removeClass('d-none');
-        else
-            $('#website-container').addClass('d-none');
-    });
-});
